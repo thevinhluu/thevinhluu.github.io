f77aa0f91c124e76463931cb6729daf809512f5b
Luu The Vinh <luuthevinh1988@gmail.com>
2020-04-01 23:37:06 +0700

    bo sung

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(function () {
+	$('.xuong, .kn').click(function () {
+		$('body, html').animate({ scrollTop: $('#khoiedu').offset().top }, 700);
+		return false;
+	});
+
+	$('.sp').click(function () {
+		$('body, html').animate({ scrollTop: $('#khoisanpham').offset().top }, 700);
+		return false;
+	});
+});
